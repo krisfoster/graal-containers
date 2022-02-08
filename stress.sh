00eb49aed9d0794adf3c5e124ba7e40f42c9de8c
kris.foster <me@krismade.me>
2022-02-08 14:55:13 +0000

    Moved to 95%

--- stress.sh
+++ stress.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 # Create my files for storing the stats in
-touch JDK_99.txt
+rm -f JDK_95.txt JDK_ALL_95.txt
+touch JDK_95.txt JDK_ALL_95.txt
 
 # Let's stress test and get some stats
 echo -ne "Stress Testing..."
 
-hey -z 15s -c 4 http://localhost:8081/jibber | grep --color=auto -Eo '99% in [0-9]+.[0-9]+ secs' > JDK_99.txt &
+hey -z 15s -c 4 http://localhost:8081/jibber | tee JDK_ALL_95.txt | grep --color=auto -Eo '95% in [0-9]+.[0-9]+ secs' > JDK_95.txt &
 
 # Progress bar
 for i in `seq 16`;
@@ -16,8 +17,8 @@ done
 echo -ne " DONE"
 echo
 
-JDK_LAT=`cat JDK_99.txt | sed 's/99% in //' | sed 's/ secs//' | awk '{printf "%d", $1*1000}'`
+JDK_LAT=`cat JDK_95.txt | sed 's/95% in //' | sed 's/ secs//' | awk '{printf "%d", $1*1000}'`
 
 echo "JDK-Container ${JDK_LAT}" \
-    | termgraph --title "Latency of 99% of Requests" --width 60 --color {green,} --suffix " ms"
+    | termgraph --title "Latency of 95% of Requests" --width 60 --color {green,} --suffix " ms"
 echo
