cd8e5afc76efcaef4bc479e3a4d3749dccfb7ba0
kris.foster <me@krismade.me>
2022-02-11 11:59:22 +0000

    Added native exe sizes

--- sizes.sh
+++ sizes.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+NATIVE_G1_SIZE=`ls -lh target/jibber-g1 | awk '{print $5}' | sed 's/M//'`
+NATIVE_SIZE=`ls -lh target/jibber | awk '{print $5}' | sed 's/M//'`
 JAR_SIZE=`ls -lh target/jibber-0.0.1-SNAPSHOT-exec.jar | awk '{print $5}' | sed 's/M//'`
 JDK_IMG_SIZE=`docker inspect -f "{{ .Size }}" localhost/jibber:jdk.0.1 | numfmt --to=si | sed 's/.$//'`
 NATIVE_IMG_SIZE=`docker inspect -f "{{ .Size }}" localhost/jibber:native.0.1 | numfmt --to=si | sed 's/.$//'`
@@ -9,9 +11,11 @@ NATIVEDISTROLESS_IMG_SIZE=`docker inspect -f "{{ .Size }}" localhost/jibber:dist
 
 # Chart of the image sizes
 echo "JAR ${JAR_SIZE}
-    JDK-Container ${JDK_IMG_SIZE}
+    Native-Exe ${NATIVE_SIZE}
+    Native-Exe-G1 ${NATIVE_G1_SIZE}
+    Distroless-Container ${NATIVEDISTROLESS_IMG_SIZE}
     NI-Container ${NATIVE_IMG_SIZE}
     NI-G1-Container ${NATIVEG1_IMG_SIZE}
-    Distroless-Container ${NATIVEDISTROLESS_IMG_SIZE}" \
+    JDK-Container ${JDK_IMG_SIZE}" \
     | termgraph --title "Container Size" --width 60 --color {green,} --suffix " MB"
 
