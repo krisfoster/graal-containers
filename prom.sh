93e1077e5407dc2b044ad989b7f8f76a4ac7e208
kris.foster <me@krismade.me>
2022-02-15 14:52:39 +0000

    Put prometheus link in own script

---
+++ prom.sh
@@ -0,0 +1,3 @@
+#/usrbin/env bash
+# Link to Prometheus
+echo "Prometheus : http://localhost:9090/graph?g0.expr=%20container_memory_rss%7Bname%3D%22jibber-jdk%22%7D&g0.tab=0&g0.stacked=0&g0.show_exemplars=0&g0.range_input=5m"
