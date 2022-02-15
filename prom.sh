c7ce7a091be16f8b349dd67f697d8b5ecbd690c5
kris.foster <me@krismade.me>
2022-02-15 15:20:49 +0000

    Moved prometheus to own script

---
+++ prom.sh
@@ -0,0 +1,2 @@
+#/usr/bin/env bash
+echo "Prometheus : http://localhost:9090/graph?g0.expr=%20container_memory_rss%7Bname%3D%22jibber-jdk%22%7D%20or%20container_memory_rss%7Bname%3D%22jibber-nibase%22%7Dor%20container_memory_rss%7Bname%3D%22jibber-nig1%22%7D&g0.tab=0&g0.stacked=0&g0.show_exemplars=0&g0.range_input=1m"
