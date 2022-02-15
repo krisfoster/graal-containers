5849fa6393495dc0ba488315fd6d14959ee6bbaa
kris.foster <me@krismade.me>
2022-02-15 14:25:50 +0000

    Moved to 2 step build for jlink contaainer

--- step-jlink.sh
+++ step-jlink.sh
@@ -7,11 +7,6 @@ VER=0.1
 CONTAINER=${REPO}/${APP}:${TYPE}.${VER}
 JAR_FILE=jibber-0.0.1-SNAPSHOT-exec.jar
 
-# Build a minimal JRE that will run our app
-rm -rf ./target/min-jre
-jlink --add-modules java.base,java.logging,java.desktop,jdk.httpserver,java.management,java.naming,java.security.jgss,java.instrument \
-        --output ./target/min-jre
-
 #echo "Building Java..."
 mvn package -DskipTests
 #echo "DONE"
